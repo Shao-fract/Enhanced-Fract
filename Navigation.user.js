7f535376b915095bffff91a734ddf0da93b67cfd
Shao <65644382+Shao-fract@users.noreply.github.com>
2024-01-13 16:24:21 +0100

    maj navigation

--- Navigation.user.js
+++ Navigation.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Navigation
 // @namespace    Go fuck yourself
-// @version      1.0
-// @description  Fuck la rose des vents
+// @version      0.2
+// @description  Fuck la rose des vents et la nav chelou
 // @author       Shao
 // @match        https://v8.fract.org/index.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fract.org
 // ==/UserScript==
+async function ficheperso(url) {
+    let response= await fetch(url)
+    let f= response.text();
+  return f;
+}
 const Nord = (e) => {
     window.location="https://v8.fract.org/index.php?act=go&quoi=n&";
 };
@@ -62,6 +67,7 @@ boutonsudest.style="position:relative;bottom:155;color:#cecece;background-color:
 
 if (document.getElementsByClassName("avatar-icon")[0].src.indexOf("png/c0")==-1)
 {
+
 document.getElementsByTagName("center")[0].prepend(boutonnordest);
 document.getElementsByTagName("center")[0].prepend(boutonnord);
 document.getElementsByTagName("center")[0].prepend(boutonnordouest);
@@ -72,3 +78,35 @@ document.getElementsByTagName("center")[0].appendChild(boutonsudest);
 else
 {
 }
+
+let perso=document.createElement('div');
+let infos=document.createElement('div');
+let groupe=document.createElement('div');
+perso.innerHTML=await ficheperso('https://v8.fract.org/p_carac.php')
+infos.innerHTML= await ficheperso('https://v8.fract.org/legende.php');
+groupe.innerHTML= await ficheperso('https://v8.fract.org/g_carac.php');
+
+let terrain=infos.getElementsByClassName('author')[0];
+let avatar=terrain.getElementsByClassName('avatar border-white')[0];
+terrain.getElementsByTagName('h4')[0].className='panel-title';
+terrain.removeChild(avatar);
+terrain.className='card';
+let people=document.getElementsByClassName("col-lg-12 col-md-12")[0].getElementsByClassName("card")[0];
+document.getElementsByClassName("col-lg-12 col-md-12")[0].removeChild(people);
+
+let charge=groupe.getElementsByClassName("col-md-6")[0].getElementsByClassName("card")[0];
+if (charge){
+}
+else{
+    charge=perso.getElementsByClassName('card')[0];
+}
+const btndemerde=document.getElementsByClassName("btn-group")[0]
+btndemerde.remove()
+//affiche perso, coef de prod et cout de dep au lieu de rose des vents
+let rose=document.getElementsByClassName('card card-user')[0];
+document.getElementsByClassName('col-md-4')[0].removeChild(rose);
+document.getElementsByClassName('col-md-4')[0].appendChild(perso.getElementsByClassName('card card-user')[0]);
+document.getElementsByClassName('col-md-4')[0].appendChild(people);
+document.getElementsByClassName('col-md-4')[0].appendChild(charge);
+document.getElementsByClassName('col-md-4')[0].appendChild(terrain);
+document.getElementsByClassName('col-md-4')[0].appendChild(infos.getElementsByClassName('card')[2]);
